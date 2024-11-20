0ae5478d33124ffbddbba93cba7217a1fb2f2083
Gary Uppal <gary.s.uppal@gmail.com>
2024-11-20 12:29:15 -0500

    update package

--- setup.py
+++ setup.py
@@ -2,8 +2,25 @@ from distutils.core import setup
 
 setup(
     name='TechniSound',
+    author='Gary Uppal',
     version='0.1dev',
     packages=['technisound',],
 #    license='Creative Commons Attribution-Noncommercial-Share Alike license',
-    long_description=open('README.txt').read(),
+    install_requires=[
+                    'numpy',
+                    'scikit-learn',
+                    'jupyterlab',
+                    'ipykernel',
+                    'matplotlib',
+                    'seaborn',
+                    'pandas',
+                    'scipy',
+                    'sounddevice'
+                    ],
+    entry_points={
+        'console_scripts': [
+            'tsound=technisound.cli:main'
+        ]
+    },
+    python_requires=">=3.8",
 )
